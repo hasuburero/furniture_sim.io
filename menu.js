ae08c6263c9e481dcdeffd9fec1d6a506844f79b
hasuburero <hasuburero0802@gmail.com>
2022-06-01 23:23:12 +0900

    edit menu.js

--- menu.js
+++ menu.js
@@ -4,8 +4,16 @@ const nav = document.getElementById("nav");
 const a =	document.getElementsByTagName('a'); 
 const Canvas3d=document.getElementById("Canvas3d");
 const CanvasFloor = document.getElementById("CanvasFloor");
+const CanvasEdit = document.getElementById("CanvasEdit");
+const CanvasAdd = document.getElementById("CanvasAdd");
+const CanvasSave = document.getElementById("CanvasSave");
+const CanvasOpen = document.getElementById("CanvasOpen");
 Canvas3d.style.display="none";
-CanvasFloor.style.display="block";
+CanvasFloor.style.display="none";
+CanvasEdit.style.display="block";
+CanvasAdd.style.display="none";
+CanvasSave.style.display="none";
+CanvasOpen.style.display="none";
 console.log(a[0]);
 
 
@@ -21,8 +29,29 @@ menu.addEventListener("click", () => {
 	}
 });
 
+back.addEventListener("click", () =>{
+	back.classList.remove("open");
+	nav.classList.remove("open-menu");
+	menu.textContent = "menu";
+});
+
+//CanvasFloor
+a[0].addEventListener("click", () =>{
+	if(CanvasEdit.style.display == "block" && CanvasAdd.style.display == "none"){
+		CanvasAdd.style.display = "block";
+		CanvasEdit.style.display = "none";
+	}
+	else{
+		CanvasAdd.style.display = "none";
+		CanvasEdit.style.display = "block";
+	}
+	nav.classList.remove("open-menu");
+	back.classList.remove("open");
+	menu.textContent = "menu";
+});
+
 a[1].addEventListener("click", () => {
-	if(Canvas3d.style.display == "none"){
+	if(CanvasEdit.style.display == "block" && Canvas3d.style.display == "none"){
 		a[1].textContent = "編集画面";
 		Canvas3d.style.display = "block";
 	}
@@ -36,8 +65,3 @@ a[1].addEventListener("click", () => {
 });
 
 
-back.addEventListener("click", () =>{
-	back.classList.remove("open");
-	nav.classList.remove("open-menu");
-	menu.textContent = "menu";
-});
